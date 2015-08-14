9c6c3e474fc20a32f9596b47ea8f86cfbb1bf260
Chris Oakley <CAOakleyII@gmail.com>
2015-08-14 11:48:00 -0400

    fixes to side bar

--- client/templates/components/side_bar.js
+++ client/templates/components/side_bar.js
@@ -1,8 +1,12 @@
 Template.side_bar.onRendered(function(){
   var routeName = Router.current().route.getName();
   $( '.' + routeName).addClass('selected');
+
   $('.menu-drawer').on('click', function() {
     $('.mobile-drawer').slideToggle();
+  });
+  $('.mobile-drawer a').on('click', function() {
+    $('.mobile-drawer').slideUp();
   });
 });
 Template.side_bar.events({
